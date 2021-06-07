ba00d384bc3ad04c438fed792229162a49fea79c
王周 <wz@yuxiit.com>
2021-06-07 16:45:10 +0800

    proto更新

--- user-service/repo/repository.go
+++ user-service/repo/repository.go
@@ -7,8 +7,8 @@ import (
 
 type Repository interface {
 	Create(user *pb.User) error
-	Get(id string) (*pb.User,error)
-	GetByEmail(email string) (*pb.User,error)
+	Get(id string) (*pb.User, error)
+	GetByEmail(email string) (*pb.User, error)
 	GetAll() ([]*pb.User, error)
 }
 
@@ -24,31 +24,30 @@ func (repo *UserRepository) Create(user *pb.User) error {
 	return nil
 }
 
-func (repo *UserRepository) Get(id string) (*pb.User,error){
+func (repo *UserRepository) Get(id string) (*pb.User, error) {
 	var user *pb.User
 	user.Id = id
 	err := repo.Db.First(&user).Error
 	if err != nil {
 		return err
 	}
-	return user,nil
+	return user, nil
 }
 
-func (repo *UserRepository) GetByEmail(email string) (*pb.User,error){
+func (repo *UserRepository) GetByEmail(email string) (*pb.User, error) {
 	user := &pb.User{}
-	err := repo.Db.Where("email = ?",email).First(&user).Error 
+	err := repo.Db.Where("email = ?", email).First(&user).Error
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return user,nil
+	return user, nil
 }
 
-
-func (repo *UserRepository) GetAll() ([]*pb.User, error){
-	vat users []*pb.User
+func (repo *UserRepository) GetAll() ([]*pb.User, error) {
+	var users []*pb.User
 	err := repo.Db.Find(&users).Error
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	return users, nil
 }
