4b6063fd3094b1ad4847288470fb0551cd17e914
王周 <wz@yuxiit.com>
2021-06-07 16:37:53 +0800

    proto更新

--- user-service/go.mod
+++ user-service/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.1.1
 	github.com/jinzhu/gorm v1.9.16
 	github.com/micro/go-micro v1.18.0
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
--- user-service/proto/user/extensions.go
+++ user-service/proto/user/extensions.go
@@ -1,10 +1,11 @@
 package laracom_user_service
 
 import (
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid := uuid.NewV4()
+	uuid := uuid.New()
 	return scope.SetColumn("Id", uuid.String())
 }
