dcb2e82d0ced6185705b853930f48ff868fc1b31
王周 <wz@yuxiit.com>
2021-06-10 11:48:50 +0800

    数据监控

--- product-service/go.mod
+++ product-service/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.5.2
 	github.com/jinzhu/gorm v1.9.16
 	github.com/micro/go-micro v1.18.0
-	github.com/zero-dora/laracom/user-service v0.0.0-20210609081345-6882f48a20fc
+	github.com/micro/go-plugins/wrapper/monitoring/prometheus v0.0.0-20200119172437-4fe21aa238fd
+	github.com/prometheus/client_golang v1.3.0
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
 )
--- product-service/main.go
+++ product-service/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/micro/go-micro"
+	"github.com/micro/go-plugins/wrapper/monitoring/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/zero-dora/laracom/product-service/db"
 	"github.com/zero-dora/laracom/product-service/handler"
 	"github.com/zero-dora/laracom/product-service/model"
 	pb "github.com/zero-dora/laracom/product-service/proto/product"
 	"github.com/zero-dora/laracom/product-service/repo"
-	"log"
 )
+
+// 启动 HTTP 服务监听客户端数据采集
+func prometheusBoot() {
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		err := http.ListenAndServe(":9092", nil)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+		}
+	}()
+}
 
 func main() {
 	// 创建数据库连接，程序退出时断开连接
@@ -18,7 +33,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not connect to DB: %v", err)
 	}
-	// 数据库迁移
+	// 数据库迁移（商品、图片、品牌、类目、属性相关数据表）
+	database.Set("gorm:table_options", "charset=utf8")
 	database.AutoMigrate(&model.Product{})
 	database.AutoMigrate(&model.ProductImage{})
 	database.AutoMigrate(&model.Brand{})
@@ -38,6 +54,7 @@ func main() {
 	srv := micro.NewService(
 		micro.Name("laracom.service.product"),
 		micro.Version("latest"), // 新增接口版本参数
+		micro.WrapHandler(prometheus.NewHandlerWrapper()),
 	)
 	srv.Init()
 
@@ -47,6 +64,9 @@ func main() {
 	pb.RegisterBrandServiceHandler(srv.Server(), &handler.BrandService{brandRepo})
 	pb.RegisterCategoryServiceHandler(srv.Server(), &handler.CategoryService{categoryRepo})
 	pb.RegisterAttributeServiceHandler(srv.Server(), &handler.AttributeService{attributeRepo})
+
+	// 采集监控数据
+	prometheusBoot()
 
 	// 启动商品服务
 	if err := srv.Run(); err != nil {
